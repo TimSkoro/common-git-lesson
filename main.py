f8ed25f26a9c3266150121f96f6256c70cc3cdaf
TimSkoro <Tsimafei.Skarakhodau@itechart-group.com>
2023-02-03 21:16:01 +0300

    feat: home page was added

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def root():
     return {"message": "Hello my best students"}
 
 
+@app.get("/home")
+def home():
+    return {"message": "Welcome to Home Page"}
+
+
 @app.get("/hello/{name}")
 def say_hello(name: str):
     return {"message": f"Hello {name}"}
